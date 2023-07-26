9666bfe284699c7189e846c99f7db802b16b6b9b
Alex Medvediev <alekmax1993@gmail.com>
2023-07-26 18:50:48 +0200

    first commit

---
+++ js/skript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?','');
+answer[1] = prompt('Как вашф фамилия?','');
+answer[2] = prompt('Сколько вам лет?','');
+
+document.write(answer);
